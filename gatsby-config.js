692151c14c670940134b6bd149a33bff037fe7de
Vignesh <vigneshwaran@positivenaick.com>
2020-09-23 12:46:52 +0530

    google analytics script added

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,31 @@
  */
 
 module.exports = {
-    plugins: [],
+    plugins: [
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                // The property ID; the tracking code won't be generated without it
+                trackingId: "UA-178786829-1",
+                // Defines where to place the tracking script - `true` in the head and `false` in the body
+                head: false,
+                // Setting this parameter is optional
+                anonymize: true,
+                // Setting this parameter is also optional
+                respectDNT: true,
+                // Avoids sending pageview hits from custom paths
+                exclude: ["/preview/**", "/do-not-track/me/too/"],
+                // Delays sending pageview hits on route update (in milliseconds)
+                pageTransitionDelay: 0,
+                // Defers execution of google analytics script after page load
+                defer: true,
+                // Any additional optional fields
+                sampleRate: 5,
+                siteSpeedSampleRate: 10,
+                cookieDomain: "vigneshr.gtsb.io",
+            },
+        },
+    ],
     siteMetadata: {
         title: "Vignesh Rajendran | Front End Developer",
         description: "I'm a front-end engineer with 3+ long stretches of experience creating site and user interfase.",
